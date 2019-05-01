351b0ff5566cfaf1468174f4c8666a449d82e0f4
matthieuClio <makabay@hotmail.fr>
2019-05-01 15:58:21 +0200

    Projet4-V-0.1
    Création des répertoires ainsi que de la page d'accueil.

---
+++ controller/accueil.php
@@ -0,0 +1,3 @@
+<?php
+	require('../view/accueil_view.php');
+?>
---
+++ view/accueil_view.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width">
+		<title>Jean Forteroche</title>
+		<meta name="description" content="Le livre Billet simple pour l'Alaska est disponible en lecture gratuite sur ce site.">
+		<link rel="icon" type="image/jpg" href="../images/icone/book.jpg">
+    	<link rel="stylesheet" href="https://necolas.github.io/normalize.css/8.0.1/normalize.css">
+    	<link rel="stylesheet" href="../css/style.css">
+	</head>
+
+	<body>
+		<header>
+
+			<!-- Menu -->
+			<nav>
+				<ul>
+					<li class="logo">
+						Jean Forteroche
+					</li>
+
+					<li class="menu">
+						Accueil
+					</li>
+					
+					<li class="login">
+						Log
+					</li>
+				</ul>
+			</nav>
+
+			<!-- Slider -->
+			<div class="slider">
+				<img src="../images/slider/fond.jpg" alt="Livre">
+				<span class="filter"></span>
+			</div>
+		</header>
+
+		<main>
+			<section>
+				<h2 class="banner">Liste des derniers chapitres publiés</h2>
+			</section>
+		</main>
+
+	</body>
+</html>
